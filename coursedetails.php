de3ce88e3d0c3b55ca82396bca15da614c574404
prath <prathamgi.pg@gmail.com>
2022-10-28 01:13:44 +0530

    initial commit

---
+++ coursedetails.php
@@ -0,0 +1,151 @@
+<?php include 'includes/header.php';?> <div class="courseMainDiv">
+   <div class="courseDiv1">
+      <h2>Courses Offered</h2>
+      <h6>Some of the courses offered for full stack web devlopment...</h6>
+      <div class="indiCourse">
+         <a href="login.php" style="text-decoration: none; color: #3e3e3e">
+            <div class="row" style="margin: 0">
+               <div class="col-lg-4 photoSection" style="background-color: #ffa089">
+                  <img src="./public/images/html5.png" />
+               </div>
+               <div class="col-lg-8 descriptionSection">
+                  <h4>Html 5</h4>
+                  <p class="duration"> Course Duration: <span>45 minutes</span>
+                  </p>
+                  <p> This course is for both beginners and seasoned developers that want to learn how to build
+                     responsive websites and user interfaces with modern HTML5{" "} </p>
+               </div>
+            </div>
+         </a>
+      </div>
+      <div class="indiCourse">
+         <a href="login.php" style="text-decoration: none; color: #3e3e3e">
+            <div class="row" style="margin: 0">
+               <div class="col-lg-4 photoSection" style="background-color: #87cefa">
+                  <img src="./public/images/css3.png" />
+               </div>
+               <div class="col-lg-8 descriptionSection">
+                  <h4>Css 3</h4>
+                  <p class="duration"> Course Duration: <span>40 minutes</span>
+                  </p>
+                  <p> CSS - short for Cascading Style Sheets - is a "programming language" you use to turn your raw HTML pages
+               into real beautiful websites. This course covers it all - we start {" "} </p>
+               </div>
+            </div>
+         </a>
+      </div>
+      <div class="indiCourse">
+         <a href="login.php" style="text-decoration: none; color: #3e3e3e">
+            <div class="row" style="margin: 0">
+               <div class="col-lg-4 photoSection" style="background-color: #87cefa">
+                  <img src="./public/images/bootstrap.png" />
+               </div>
+               <div class="col-lg-8 descriptionSection">
+                  <h4>Bootstrap</h4>
+                  <p class="duration"> Course Duration: <span>60 minutes</span>
+                  </p>
+                  <p> This course will literally take you from knowing nothing about Bootstrap 3 or 4 to learning all of the
+            utilities, components, widgets and grids and building real world themes and websites. {" "} </p>
+               </div>
+            </div>
+         </a>
+      </div>
+      <div class="indiCourse">
+         <a href="login.php" style="text-decoration: none; color: #3e3e3e">
+            <div class="row" style="margin: 0">
+               <div class="col-lg-4 photoSection" style="background-color: #fbec5d">
+                  <img src="./public/images/js.png" />
+               </div>
+               <div class="col-lg-8 descriptionSection">
+                  <h4>JavaScript</h4>
+                  <p class="duration"> Course Duration: <span>2.5 hours</span>
+                  </p>
+                  <p> JavaScript is the most popular programming language in the world. It powers the entire modern web. It
+            provides millions of high-paying jobs all over the world. That's why you  {" "} </p>
+               </div>
+            </div>
+         </a>
+      </div>
+      <div class="indiCourse">
+         <a href="login.php" style="text-decoration: none; color: #3e3e3e">
+            <div class="row" style="margin: 0">
+               <div class="col-lg-4 photoSection" style="background-color: #74c365">
+                  <img src="./public/images/nodejs.png" />
+               </div>
+               <div class="col-lg-8 descriptionSection">
+                  <h4>Node JS</h4>
+                  <p class="duration"> Course Duration: <span>1.5 hours</span>
+                  </p>
+                  <p> Node.js is probably THE most popular and modern server-side programming language you can dive into these
+            days! Node.js developers are in high demand and the language is used for everything {" "} </p>
+               </div>
+            </div>
+         </a>
+      </div>
+      <div class="indiCourse">
+         <a href="login.php" style="text-decoration: none; color: #3e3e3e">
+            <div class="row" style="margin: 0">
+               <div class="col-lg-4 photoSection" style="background-color: #d3d3d3">
+                  <img src="./public/images/express.png" />
+               </div>
+               <div class="col-lg-8 descriptionSection">
+                  <h4>Express JS</h4>
+                  <p class="duration"> Course Duration: <span>1 hour</span>
+                  </p>
+                  <p> First things you're going to learn in any node course is about the Express module. But
+            how much do you really learn about it? How to render a page in one template engine? How to make a get and a
+            post route?{" "} </p>
+               </div>
+            </div>
+         </a>
+      </div>
+      <div class="indiCourse">
+         <a href="login.php" style="text-decoration: none; color: #3e3e3e">
+            <div class="row" style="margin: 0">
+               <div class="col-lg-4 photoSection" style="background-color: #696969">
+                  <img src="./public/images/react.png" />
+               </div>
+               <div class="col-lg-8 descriptionSection">
+                  <h4>React JS</h4>
+                  <p class="duration"> Course Duration: <span>2 hours</span>
+                  </p>
+                  <p> React.js is THE most popular JavaScript library you can use and learn these days to build modern, reactive
+            user interfaces for the web. This course teaches you React in-depth, from the ground up, step by step by
+            {" "} </p>
+               </div>
+            </div>
+         </a>
+      </div>
+      <div class="indiCourse">
+         <a href="login.php" style="text-decoration: none; color: #3e3e3e">
+            <div class="row" style="margin: 0">
+               <div class="col-lg-4 photoSection" style="background-color: #353839">
+                  <img src="./public/images/angular.png" />
+               </div>
+               <div class="col-lg-8 descriptionSection">
+                  <h4>Angular JS</h4>
+                  <p class="duration"> Course Duration: <span>2.5 hours</span>
+                  </p>
+                  <p> Angular 12 simply is the latest version of Angular 2, you will learn this amazing framework from the ground
+            up in this course! Join the most comprehensive, popular and bestselling Angular course on divstack{" "} </p>
+               </div>
+            </div>
+         </a>
+      </div>
+      <div class="indiCourse">
+         <a href="login.php" style="text-decoration: none; color: #3e3e3e">
+            <div class="row" style="margin: 0">
+               <div class="col-lg-4 photoSection" style="background-color: #d0f0c0">
+                  <img src="./public/images/mongodb.png" />
+               </div>
+               <div class="col-lg-8 descriptionSection">
+                  <h4>Mongo DB</h4>
+                  <p class="duration"> Course Duration: <span>4 hours</span>
+                  </p>
+                  <p> MongoDB is one of the most important NoSQL databases you can work with these days. It's extremely popular and MongoDB developers are in high demand. {""}</p>
+               </div>
+            </div>
+         </a>
+      </div>
+   </div>
+</div> <?php include 'includes/footer.php';?>
